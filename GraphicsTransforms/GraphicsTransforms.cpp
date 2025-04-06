2f238b8f922e346868ab403e9bea8c2908a93d7a
Kostiantyn Tokar <tokar.kostya@gmail.com>
2025-04-06 11:24:17 +0300

    Initial commit

---
+++ GraphicsTransforms/GraphicsTransforms.cpp
@@ -0,0 +1,12 @@
+﻿// GraphicsTransforms.cpp : Defines the entry point for the application.
+//
+
+#include "GraphicsTransforms.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello CMake." << endl;
+    return 0;
+}
